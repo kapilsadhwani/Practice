b4efbce21d5480d4a86ff05a056a6d321530e7af
kapilsadhwani <kapil.sadhwani@spacetimeinsight.com>
2020-07-01 04:42:45 -0700

    Added floodFill method

--- AlgoAndDS/src/com/implement/graph/PaintFill.java
+++ AlgoAndDS/src/com/implement/graph/PaintFill.java
@@ -1,5 +1,7 @@
 package com.implement.graph;
 
+import java.util.Arrays;
+
 public class PaintFill {
 	private int[] rowoffsets = {0, 1, 0, -1};
 	private int[] coloffsets = {1, 0 ,-1, 0};
@@ -34,5 +36,45 @@ public class PaintFill {
 			}
 		}
 		return true;
+	}
+	
+	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+		if( sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length) 
+			return image;
+		
+		if (image[sr][sc] == newColor)
+			return image;
+		
+		floodFill(image, sr, sc, image[sr][sc], newColor);
+		
+		return image;
+	}
+	
+	private void floodFill(int[][] image, int sr, int sc, int ocolor, int ncolor) {
+		image[sr][sc] = ncolor;
+
+		for (int i = 0; i < rowoffsets.length; i++) {
+			int newX = sr + rowoffsets[i];
+			int newY = sc + coloffsets[i];
+
+			if (newX < 0 || newX >= image.length || newY < 0
+					|| newY >= image[0].length)
+				continue;
+
+			if (image[newX][newY] == ocolor) {
+				floodFill(image, newX, newY, ocolor, ncolor);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		PaintFill pf = new PaintFill();
+		int[][] image = {{1,1,1},{1,1,0},{1,0,1}};
+		int sr = 1, sc = 1, newColor = 2;
+		
+		int[][] result = pf.floodFill(image, sr, sc, newColor);
+
+		for (int i = 0; i < result.length; i++)
+			System.out.println(Arrays.toString(result[i]));
 	}
 }
